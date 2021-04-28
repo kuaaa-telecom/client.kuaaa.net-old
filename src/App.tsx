ad2347b1bd99afc0260d40c4237daaa9c35c3191
hasu <jun2077681@naver.com>
2021-04-28 16:06:56 +0900

    로그인 하는 부분, 타이틀 부분 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import LoginBox from "./components/Login/LoginBox";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return (
-    <div>
-      HELLO KUAAA
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path={"/"} component={LoginPage}/>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
