3ec5271ed8198b549e1f426d4667ce8c5c9f16ff
PngWnA <2016320146@korea.ac.kr>
2021-07-15 02:21:53 +0900

    [Core/fullpage] Removed unused code

--- src/components/Login/LoginBoxBottom.tsx
+++
@@ -1,9 +0,0 @@
-import "./LoginBoxBottom.scss"
-
-export default function LoginBoxBottom() {
-    return (
-        <div className={"login-box-bottom"}>
-            KUAAA란? | 회원가입 | 비밀번호 찾기
-        </div>
-    )
-}
