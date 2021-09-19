83375f2ced73517b4d521e02765f6990caee1a2d
PngWnA <2016320146@korea.ac.kr>
2021-09-19 14:16:52 +0900

    [Auth] Implemented Login panel

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import {Flex, Box, Heading, FormControl, FormLabel, Stack, Checkbox, Link, Button, Input} from '@chakra-ui/react';
+import React, {useState} from 'react';
+import {Flex, Box, Heading, FormControl, FormLabel, Stack, Checkbox, Link, Button, Input, CircularProgress, Text} from '@chakra-ui/react';
 
-const LoginArea = () => {
+
+export const LoginArea = () => {
   return (
     <Flex minHeight='100vh' width='full' align="center" justifyContent="center">
       <Box
         borderWidth={1}
         px={4}
+        py={4}
         width='full'
         maxWidth='500px'
         borderRadius={4}
@@ -31,16 +33,59 @@ const LoginHeader = () => {
 };
 
 const LoginForm = () => {
+  const [id, setid] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isLoading, setisLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loginHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setisLoading(true);
+
+    const response = await fetch(
+        'http://localhost:31413/auth/login'
+        , {
+          method: 'POST',
+          body: JSON.stringify({id, password}),
+          headers: {'Content-Type': 'application/json'},
+          mode: 'cors',
+        },
+    );
+    const data = await response.json();
+    if (data.token) {
+      setIsLoggedIn(true);
+      setError('');
+      localStorage.setItem('token', data.token);
+    } else {
+      setError(data.msg);
+    }
+    setisLoading(false);
+  };
   return (
     <Box my={4} textAlign='left'>
-      <form>
-        <FormControl>
-          <FormLabel>Email</FormLabel>
-          <Input type="email" placeholder="sirius@kuaaa.net" />
+      {isLoggedIn ? (
+          <Box textAlign="center">
+            <Text my={4} >Hello <Text as="span" color="yellow.200">{id}</Text></Text>
+            <Text fontSize="md">kuaaa.net is coming soon @ December 2021</Text>
+            <Button
+              variant="outline"
+              width="full"
+              mt={4}
+              onClick={() => setIsLoggedIn(false)}
+            >
+            Sign out
+            </Button>
+          </Box>
+      ) : (
+      <form onSubmit={loginHandler}>
+        <Text color="cyan" align="center">{error}</Text>
+        <FormControl isRequired>
+          <FormLabel>ID</FormLabel>
+          <Input type="id" placeholder="sirius" onChange={(event) => setid(event.currentTarget.value)}/>
         </FormControl>
-        <FormControl mt={2}>
+        <FormControl mt={2} isRequired>
           <FormLabel>Password</FormLabel>
-          <Input type="password" placeholder="brightest start in the sky" />
+          <Input type="password" placeholder="brightest start in the sky" onChange={(event) => setPassword(event.currentTarget.value)}/>
         </FormControl>
 
         <Stack isInline justifyContent="space-between" mt={6}>
@@ -52,8 +97,15 @@ const LoginForm = () => {
           </Box>
         </Stack>
 
-        <Button width="full" mt={4}>Sign In</Button>
-      </form>
+        <Button width="full" mt={4} type="submit" >
+          {isLoading ? (
+          <CircularProgress isIndeterminate size="24px" color="teal" />
+          ) : (
+          'Sign In'
+          )}
+        </Button>
+      </form>)}
+
     </Box>
   );
 };
@@ -61,9 +113,8 @@ const LoginForm = () => {
 const LoginFooter = () => {
   return (
     <Box>
-      Korea University. Amateur Astronomical Association
+      <Text color="#dc143c" as="b" opacity={0.8}>고려대학교 아마추어 천문회<br/></Text>
+      <Text color="#dc143c" as="b" opacity={0.8}>Korea University Amateur Astronomical Association</Text>
     </Box>
   );
 };
-
-export {LoginArea};
