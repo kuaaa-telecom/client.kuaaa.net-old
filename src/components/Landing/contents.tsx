4c48c8b1e040d784fb43b415342cb5ecf3c1b043
PngWnA <gnu@korea.ac.kr>
2021-10-08 12:26:54 +0900

    [Text] Polished contents

--- src/components/Landing/contents.tsx
+++ src/components/Landing/contents.tsx
@@ -32,17 +32,7 @@ export const About = () => {
       <Box my={20}>
         <Text fontSize="md" fontWeight="medium" my={5}>
           <Text as="b">KUAAA</Text>는 <Text as="b">K</Text>orea <Text as="b">U</Text>niversity <Text as="b">A</Text>mateur <Text as="b">A</Text>stronomical <Text as="b">A</Text>ssociation의 약자로,
-          밤하늘의 별을 좋아하는 사람들이 모인 <Text as="b">고려대학교 유일의 천문동아리</Text>입니다.
-        </Text>
-        <Text my={5}>
-          <Text as="b">KUAAA는 모두에게 열려 있습니다!</Text> 함께 관측회에서 쏟아지는 별들과 은하수를 구경하시는건 어떨까요?
-          별에 대해 아는게 적더라도 걱정하지 마세요. 다양한 세미나를 통해 별과 밤하늘에 대해 알려드립니다.
-        </Text>
-        <Text my={5}>
-          별 보는 것 외에도, 동아리 소유 카메라를 가지고 출사를 가기도 하고, 넓고 쾌적한 동아리방에서 보드게임도 하고 술도 마시며 사람들과 금방 친해질 수도 있습니다.
-        </Text>
-        <Text my={5}>
-          이렇듯 다채로운 활동을 하는 저희 동아리는현재 코로나로 인해 일부 활동을 온라인으로 전환했지만, 여전히 다양한 사람들과 함께 즐거운 동아리 생활을 즐길 수 있습니다.
+          별을 좋아하는 사람들이 모인 <Text as="b">고려대학교 유일의 천문동아리</Text>입니다.
         </Text>
       </Box>
     </Box>
@@ -64,14 +54,13 @@ export const Observation = () => {
       </Box>
       <Box my={20}>
         <Text fontSize="md" fontWeight="medium" my={5}>
-        매 달 달이 제일 어두운 주말, 서울을 떠나 1박 2일로 정기 관측회를 떠납니다.
+        매달 달이 제일 어두운 주말, 서울을 벗어나 1박 2일로 <Text as="b">정기 관측회</Text>를 떠납니다.
         </Text>
         <Text fontSize="md" fontWeight="medium" my={5}>
-          정기 관측회에서는 동아리 장비로 예쁜 밤하늘을 사진으로 찍어 추억을 남길 수도 있고, 수많은 별들을 눈으로 직접 감상할 수도 있습니다.
-          직접 찍은 사진으로 다른 사람들과 추억을 공유할 수도 있습니다.
+          <Text as="b">정기 관측회</Text>에서는 예쁜 밤하늘을 사진으로 찍어 추억을 남길 수도 있고, 수많은 별들을 눈으로 직접 감상할 수도 있습니다.
         </Text>
         <Text fontSize="md" fontWeight="medium" my={5}>
-        일정이 맞지 않아 정기 관측회를 갈 수 없더라도 걱정 마세요! 날씨가 맑은 밤 당일치기로 학교를 떠나는 비정기 관측회에도 참가할 수 있답니다.
+        일정이 맞지 않아 정기 관측회를 갈 수 없더라도, 날씨가 맑은 밤 학교를 떠나는 <Text as="b">비정기 관측회</Text>에 참여할 수 있습니다.
         </Text>
       </Box>
     </Box>
@@ -93,13 +82,13 @@ export const Seminar = () => {
       </Box>
       <Box my={20}>
         <Text fontSize="md" fontWeight="medium" my={5}>
-          관측 행사 외에도 천문학 지식을 넓힐 수 있는 학술 세미나가 준비되어 있어요.
+          관측 행사 외에도 천문학 지식을 넓힐 수 있는 <Text as="b">학술 세미나</Text>가 준비되어 있습니다.
         </Text>
         <Text fontSize="md" fontWeight="medium" my={5}>
-          동아리 장비 사용법, 유성우, 별자리와 같은 기초적인 주제부터 외계인, 우주탐사, 화성개척와 같은 재미있는 주제의 세미나가 한 학기에 다섯 번 이상 진행됩니다.
+        별자리, 관측 장비와 같은 기초적인 이론부터, 외계인, 우주탐사, 화성 개척과 같은 다양한 주제의 세미나가 한 학기에 다섯 번 이상 진행됩니다.
         </Text>
         <Text fontSize="md" fontWeight="medium" my={5}>
-          세미나를 들으면서 멀게만 느껴졌던 아마추어 천문학에 한 발짝 더 다가갈 수 있습니다.
+        세미나를 들으면서 어렵게만 느껴지는 천문학에 한발짝 더 다가갈 수 있습니다.
         </Text>
       </Box>
     </Box>
@@ -111,23 +100,20 @@ export const Exhibition = () => {
     <Box maxW="45%" mx="5%">
       <Box>
         <Text fontSize="7xl" fontWeight="bold">
-            천체사진전
+        천체사진전
         </Text>
       </Box>
       <Box my={-5}>
         <Text fontSize="2xl" fontWeight="semibold">
-            추억나누기
+        추억 나누기
         </Text>
       </Box>
       <Box my={20}>
         <Text fontSize="md" fontWeight="medium" my={5}>
-          매년 연말에 KUAAA에서는 정기 천체사진전을 진행해요.
+        매년 연말, KUAAA에서는 정기 천체사진전을 진행합니다.
         </Text>
         <Text fontSize="md" fontWeight="medium" my={5}>
-        연말에 열리는 정기 천체사진전을 통해 동아리에서 찍었던 사진들과 쌓았던 추억들을 공유할 수 있답니다!
-        </Text>
-        <Text fontSize="md" fontWeight="medium" my={5}>
-        동아리에서 배운 천체관측 실력을 사진전을 통해서 모두에게 자랑할 수 있어요.
+        정기 천체사진전을 통해 동아리에서 찍었던 사진을 전시하고, 동아리의 한 해를 정리합니다.
         </Text>
       </Box>
     </Box>
@@ -138,7 +124,7 @@ export const Etc = () => {
   return (
     <Box maxW="45%" mx="5%">
       <Box>
-        <Text fontSize="7xl" fontWeight="bold">
+        <Text fontSize="6xl" fontWeight="bold">
             출사, MT, 합숙
         </Text>
       </Box>
@@ -149,19 +135,13 @@ export const Etc = () => {
       </Box>
       <Box my={20}>
         <Text fontSize="md" fontWeight="medium">
-          별에 관심이 없으신 분도 환영합니다!
-        </Text>
-        <Text fontSize="md" fontWeight="medium">
-          KUAAA는 별만 보는 동아리가 아니니까요!
+        천문 관련 행사 외에 동아리원들과 함께 사진을 찍으러 떠나는 <Text as="b">출사</Text>와,
         </Text>
         <Text fontSize="md" fontWeight="medium" mt={5}>
-        천문학 관련 행사 외에도 사진 찍는 소풍 분위기의 출사와,
+          <Text as="b">총회</Text> 및 <Text as="b">세미나 뒤풀이</Text> 자리에서 친목을 다질 수 있습니다.
         </Text>
-        <Text fontSize="md" fontWeight="medium">
-        각종 총회 및 세미나 뒤풀이 자리에서 친목을 다질 수도 있습니다!
-        </Text>
-        <Text fontSize="md" fontWeight="medium" mt={10}>
-        그리고 학기가 끝나면 2박 3일의 엠티와 합숙 행사에서 방학을 즐겨 보아요.
+        <Text fontSize="md" fontWeight="medium" mt={5}>
+        매 학기가 끝나면 방학 중의 2박 3일의 <Text as="b">MT</Text>와 <Text as="b">합숙</Text> 행사를 즐길 수도 있습니다.
         </Text>
       </Box>
     </Box>
