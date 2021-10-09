6ce86d5fedb2213e53b550dfda7320f747006ac4
PngWnA <2016320146@korea.ac.kr>
2021-10-09 22:14:14 +0900

    [Landing] Tuned positioning of the LTextBox

--- src/components/Landing/contents.tsx
+++ src/components/Landing/contents.tsx
@@ -16,21 +16,53 @@ export const Header = () => {
   );
 };
 
+const opacity = 0.8;
+
+type LTextBoxProp = {
+  title: JSX.Element,
+  subtitle: JSX.Element,
+  maintext: JSX.Element,
+};
+export const LTextBox = ({title, subtitle, maintext}: LTextBoxProp) => {
+  const opacity = 0.8;
+  const fromLeft = '25%';
+  const boxWidth = '25%';
+  return (
+    <Box mx={fromLeft} maxW={boxWidth}>
+      <Box>
+        <Text fontSize="7xl" fontWeight="bold">
+          {title}
+        </Text>
+      </Box>
+      <Box my={-5}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
+          {subtitle}
+        </Text>
+      </Box>
+      <Box my={20}>
+        <Text fontSize="md" fontWeight="medium">
+          {maintext}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
 export const About = () => {
   return (
-    <Box maxW="45%" mx="5%">
+    <Box maxW="25%" mx="20%">
       <Box>
         <Text fontSize="7xl" fontWeight="bold">
           KUAAA
         </Text>
       </Box>
-      <Box my={-7}>
-        <Text fontSize="2xl" fontWeight="semibold">
+      <Box my={-5}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
           고려대학교 아마추어 천문회
         </Text>
       </Box>
       <Box my={20}>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium">
           <Text as="b">KUAAA</Text>는 <Text as="b">K</Text>orea <Text as="b">U</Text>niversity <Text as="b">A</Text>mateur <Text as="b">A</Text>stronomical <Text as="b">A</Text>ssociation의 약자로,
           별을 좋아하는 사람들이 모인 <Text as="b">고려대학교 유일의 천문동아리</Text>입니다.
         </Text>
@@ -41,25 +73,25 @@ export const About = () => {
 
 export const Observation = () => {
   return (
-    <Box maxW="45%" mx="5%">
-      <Box>
+    <Box maxW="25%" mx="20%">
+      <Box ml={-1}>
         <Text fontSize="7xl" fontWeight="bold">
           관측회
         </Text>
       </Box>
-      <Box my={-5}>
-        <Text fontSize="2xl" fontWeight="semibold">
+      <Box my={-1}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
           밤하늘, 별, 은하수
         </Text>
       </Box>
       <Box my={20}>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium">
         매달 달이 제일 어두운 주말, 서울을 벗어나 1박 2일로 <Text as="b">정기 관측회</Text>를 떠납니다.
         </Text>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium" mt={5}>
           <Text as="b">정기 관측회</Text>에서는 예쁜 밤하늘을 사진으로 찍어 추억을 남길 수도 있고, 수많은 별들을 눈으로 직접 감상할 수도 있습니다.
         </Text>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium" mt={5}>
         일정이 맞지 않아 정기 관측회를 갈 수 없더라도, 날씨가 맑은 밤 학교를 떠나는 <Text as="b">비정기 관측회</Text>에 참여할 수 있습니다.
         </Text>
       </Box>
@@ -69,14 +101,14 @@ export const Observation = () => {
 
 export const Seminar = () => {
   return (
-    <Box maxW="45%" mx="5%">
-      <Box>
+    <Box maxW="25%" mx="20%">
+      <Box ml={-1}>
         <Text fontSize="7xl" fontWeight="bold">
             세미나
         </Text>
       </Box>
-      <Box my={-5}>
-        <Text fontSize="2xl" fontWeight="semibold">
+      <Box my={-1}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
             천문학과 친해지기
         </Text>
       </Box>
@@ -97,22 +129,22 @@ export const Seminar = () => {
 
 export const Exhibition = () => {
   return (
-    <Box maxW="45%" mx="5%">
-      <Box>
+    <Box maxW="25%" mx="20%">
+      <Box ml={-1.5}>
         <Text fontSize="7xl" fontWeight="bold">
         천체사진전
         </Text>
       </Box>
-      <Box my={-5}>
-        <Text fontSize="2xl" fontWeight="semibold">
+      <Box my={-1}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
         추억 나누기
         </Text>
       </Box>
       <Box my={20}>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium" mt={5}>
         매년 연말, KUAAA에서는 정기 천체사진전을 진행합니다.
         </Text>
-        <Text fontSize="md" fontWeight="medium" my={5}>
+        <Text fontSize="md" fontWeight="medium" mt={5}>
         정기 천체사진전을 통해 동아리에서 찍었던 사진을 전시하고, 동아리의 한 해를 정리합니다.
         </Text>
       </Box>
@@ -122,14 +154,14 @@ export const Exhibition = () => {
 
 export const Etc = () => {
   return (
-    <Box maxW="45%" mx="5%">
-      <Box>
+    <Box maxW="25%" mx="20%">
+      <Box ml={-1}>
         <Text fontSize="6xl" fontWeight="bold">
             출사, MT, 합숙
         </Text>
       </Box>
-      <Box my={-5}>
-        <Text fontSize="2xl" fontWeight="semibold">
+      <Box my={-1}>
+        <Text fontSize="2xl" fontWeight="semibold" opacity={opacity}>
             기타 행사들
         </Text>
       </Box>
